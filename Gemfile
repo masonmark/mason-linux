7a348ba0b8dd1030ded9eb0ddeddd84492e0fef6
Mason Mark <mason@fivespeed.com>
2016-05-31 10:12:32 +0900

    Rewrite deploy script based on Derployer™ (from mason.gem, which is now required).

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'mason', :github => 'masonmark/mason-gem'
--- bin/mason_linux_deployer.rb
+++ bin/mason_linux_deployer.rb
@@ -2,101 +2,41 @@
 #
 # A deploy script to drive Ansible to deploy Mason Linux onto a compatible host (which means CentOS 7 only, at least as of 2016-05-02).
 
-require_relative '../lib/cli.rb'
-require_relative '../lib/io.rb'
+unless ENV['BUNDLE_GEMFILE']
+  puts "  ☠   "
+  puts "  ☠   ERROR: BUNDLER ENVIRONMENT REQUIRED."
+  puts "  ☠   "
+  puts "  ☠   It seems you are not using Bundler to"
+  puts "  ☠   invoke this script. It is required."
+  puts "  ☠   "
+  puts "  ☠   Please re-run this script something like:"
 
-require 'pathname'
-require 'tempfile'
-require 'yaml'
+  puts ""
+  puts "       bundle exec #{$PROGRAM_NAME} #{ARGV}"
 
-PATH_TO_ME      = Pathname.new(File.expand_path(__FILE__))
-PATH_TO_ROOT    = PATH_TO_ME.parent.parent
-INVOCATION_NAME = PATH_TO_ME.basename
+  puts "  ☠   "
+  abort()
+end
 
 
-POSSIBLE_ARGS = {
-  reset:       "Remove any stored 'last-used' settings (will cause defaults to be used).",
-}
+require 'mason'
 
-VALID_SETTINGS = {
-  # This list allows crude user-input validation to be done, rejecting invalid input.
-  ansible_playbook:    ['ansible/site.yml', 'ansible/site-debug.yml'],
-  server_type:         ['vmware-fusion', 'generic'],
-}
 
-DEFAULT_SETTINGS = {
-  ansible_playbook: 'ansible/site.yml',
+
+definitions = {
+  ansible_playbook: ['ansible/site.yml', 'ansible/site-debug.yml'],
+  server_type: ['generic', 'vmware-fusion'],
   override_ssh_key: 'key',
   rails_app_name: 'my_cool_rails_app',
-  server_type: 'generic',
-  sysadmin_user_name: 'centos',
+  sysadmin_username: 'centos',
   target_host: "1.2.3.4",
   target_ssh_port: 22,
 }
 
+derp = Mason::Derployer.new 'mason-linux-deployer'
 
-class MasonLinuxDeployer
-
-  def initialize
-    @settings = DEFAULT_SETTINGS
-  end
-
-
-  def main(args=ARGV)
-      greet_user
-      process_args
-      confirm_settings
-      settings_write
-      perform_work
-  end
-
-
-  def build_command_for_playbook(playbook_name)
-		key_src = @settings[:override_ssh_key]
-		if [nil, ''].include? key_src
-			key_src = :auto
-		end
-
-    private_key_file = write_private_key_to_temp_file key_src
-
-    inventory_content = "[#{@settings[:server_type]}]\ndeploy_target ansible_ssh_host=#{@settings[:target_host]} ansible_ssh_port=#{@settings[:target_ssh_port]}"
-
-    inventory_file = Tempfile.new 'inventory'
-    inventory_file.write inventory_content
-    inventory_file.close
-
-    extra_vars  = "--extra-vars \""
-    extra_vars += "sysadmin_username='#{@settings[:sysadmin_user_name]}' "
-    extra_vars += "server_type='#{@settings[:server_type]}' "
-    extra_vars += "rails_app_name='#{@settings[:rails_app_name]}' "
-    extra_vars += "\" "
-
-    command  = "ansible-playbook " # Mason 2016-03-15: you can add -vvvvv here to debug ansible troubles.
-    command += "--inventory-file='#{inventory_file.path}' "
-    command += "--user='#{@settings[:sysadmin_user_name]}' "
-    command += "--private-key='#{private_key_file}' "
-    # command += "--ask-vault-pass " if @settings[:server_type] == 'production'
-    command += extra_vars
-    command += "#{playbook_name}"
-
-    command
-  end
-
-
-  def perform_work
-    command = "#{build_command_for_playbook @settings[:ansible_playbook]}"
-
-    puts "\nATTEMPTING TO DEPLOY VIA ANSIBLE AS FOLLOWS:\n"
-    puts command
-
-    Dir.chdir PATH_TO_ROOT
-
-    Kernel.system "time #{command}"
-  end
-
+definitions.each do |k, v|
+  derp.define k => v
 end
 
-
-if __FILE__ == $PROGRAM_NAME
-  me = MasonLinuxDeployer.new.main
-end
+derp.run
